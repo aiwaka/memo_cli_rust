adc8af4fa38cec6bc26a07dbcc77e8841342ceb0
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-06 03:08:20 +0900

    enable to expand the tilde of specified path

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,53 @@
+use std::path::PathBuf;
+
+use home_dir::HomeDirExt;
 use serde::Deserialize;
 
 #[derive(Debug)]
 pub struct AppEnv {
     pub config_path: String,
 }
+impl AppEnv {
+    pub fn get_config_fullpath(&self) -> PathBuf {
+        self.config_path
+            .expand_home()
+            .unwrap()
+            .canonicalize()
+            .unwrap()
+    }
+}
 
+/// 設定ファイルからの読み込み用構造体
 #[derive(Debug, Deserialize)]
-pub struct AppConfig {
+pub struct AppConfigInput {
     pub storage_dir: String,
     pub server_port: u16,
+}
+
+#[derive(Debug)]
+pub struct AppConfig {
+    /// チルダとかを含むことができるユーザー設定パス
+    pub storage_dir: String,
+    /// 完全に展開されたパス
+    pub full_storage_dir: String,
+    pub server_port: u16,
+}
+
+impl AppConfig {
+    pub fn new(data: AppConfigInput) -> Self {
+        let full_storage_dir = data
+            .storage_dir
+            .expand_home()
+            .unwrap()
+            .canonicalize()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
+        Self {
+            storage_dir: data.storage_dir,
+            full_storage_dir,
+            server_port: data.server_port,
+        }
+    }
 }
--- src/load.rs
+++ src/load.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::error::Error;
 use std::fs::read_to_string;
 
-use crate::config::{AppConfig, AppEnv};
+use crate::config::{AppConfig, AppConfigInput, AppEnv};
 use crate::{APP_CONFIG, APP_ENV};
 
 pub(crate) fn load_config() -> Result<(), Box<dyn Error>> {
@@ -14,12 +14,10 @@ pub(crate) fn load_config() -> Result<(), Box<dyn Error>> {
 
     APP_ENV.set(AppEnv { config_path }).unwrap();
 
-    println!("{}", APP_ENV.get().unwrap().config_path);
+    let app_config_str = read_to_string(APP_ENV.get().unwrap().get_config_fullpath())?;
+    let app_config_input: AppConfigInput = toml::from_str(&app_config_str)?;
 
-    let app_config_str = read_to_string(APP_ENV.get().unwrap().config_path.clone())?;
-    let app_config: AppConfig = toml::from_str(&app_config_str)?;
-
-    APP_CONFIG.set(app_config).unwrap();
+    APP_CONFIG.set(AppConfig::new(app_config_input)).unwrap();
 
     Ok(())
 }
