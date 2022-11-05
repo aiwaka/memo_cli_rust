91eb57ba63ff26c85a42581df8b3d11edd91b9f6
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-05 19:49:49 +0900

    add crate and list format

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,19 +1,38 @@
+use unicode_width::UnicodeWidthStr;
+
 use crate::{
     io::extract_first_line,
     memo_list::{memo_fullpath_list, memo_name_list},
 };
 
+/// 指定された幅を超えない最大の大きさの部分文字列を返す
+fn get_substring_of_limited_width(text: &str, target: usize) -> &str {
+    for i in 1..text.len() {
+        let slice_end = text.char_indices().nth(i).unwrap().0;
+        let width = (text[0..slice_end]).width_cjk();
+        if width > target {
+            return &text[0..slice_end];
+        }
+    }
+    ""
+}
+
 pub(super) fn list_memos(full: &bool) {
     if *full {
         let memo_list = memo_fullpath_list();
         for (title, fullpath) in memo_list.iter() {
-            println!("{}: {}", title, fullpath.to_str().unwrap());
+            println!(
+                "{:width$}: {}",
+                title,
+                fullpath.to_str().unwrap(),
+                width = 6
+            );
         }
     } else {
         let memo_list = memo_name_list();
         for title in memo_list.iter() {
             let first_line = extract_first_line(title).unwrap();
-            println!("{}: {}", title, first_line);
+            println!("{:<width$}: {}", title, first_line, width = 16);
         }
     }
 }
