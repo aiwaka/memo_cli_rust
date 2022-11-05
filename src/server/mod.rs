445fca1893af1ed0ba1a087950dda2c7ee070961
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-06 00:45:39 +0900

    modify handling template

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
@@ -18,6 +17,19 @@ pub(crate) fn http_server() {
         handle_connection(stream);
     }
 }
+
+/// templateディレクトリからhtml文字列を取得する
+fn get_html_from_template(name: &str, html_buf: &mut String) {
+    let path_str = format!("src/server/template/{}.html", name);
+    let path = Path::new(&path_str);
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+    let mut reader = BufReader::new(file);
+    reader.read_to_string(html_buf).unwrap();
+}
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
@@ -25,23 +37,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let mut html_buf = String::new();
     let response = if buffer.starts_with(get) {
-        let mut html_buf = String::new();
-        // let current_dir = env::current_exe().unwrap();
-        let path = Path::new("src/server/asset/index.html");
-        let file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", path.display(), why),
-            Ok(file) => file,
-        };
-        let mut reader = BufReader::new(file);
-        reader.read_to_string(&mut html_buf).unwrap();
-
+        get_html_from_template("index", &mut html_buf);
         let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", html_buf);
         response
     } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n<html>Not Found</html>".to_string()
+        get_html_from_template("404", &mut html_buf);
+        format!("HTTP/1.1 404 Not Found\r\n\r\n{}", html_buf)
     };
-    #[allow(clippy::unused_io_amount)]
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
