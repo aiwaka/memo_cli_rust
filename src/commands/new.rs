a0d0a7937d18bf6c5c1e69c2e5560f38447aa636
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-05 16:55:11 +0900

    externalize general method

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,30 +1,12 @@
+use crate::io::{create_new_file, input_simple_text};
 use std::error::Error;
-use std::io::{stdin, stdout, Write};
-
-use crate::io::create_new_file;
-
-fn input_name() -> String {
-    let mut name = String::new();
-    print!("Input Name: ");
-    stdout().flush().unwrap();
-    stdin()
-        .read_line(&mut name)
-        .expect("could not read string.");
-    if let Some('\n') = name.chars().next_back() {
-        name.pop();
-    }
-    if let Some('\r') = name.chars().next_back() {
-        name.pop();
-    }
-    name
-}
 
 pub(super) fn new_command(name: &Option<String>) -> Result<(), Box<dyn Error>> {
     let name = if let Some(name) = name {
         name.clone()
     } else {
         // 名前が指定されていないなら聞く
-        input_name()
+        input_simple_text("Input title: ")
     };
     create_new_file(&name)?;
 
