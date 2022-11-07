8469fd5c204c23cc608cf2ec51c4608a287e0f62
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-07 14:31:14 +0900

    add comment

--- src/server/open_browser.rs
+++ src/server/open_browser.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
+use std::process::Command;
 
+/// 指定されたurlをブラウザで開く
 pub(crate) fn open_browser(url: String) -> Result<(), Box<dyn Error>> {
-    use std::process::Command;
-
+    // osごとにコマンドが違うのでビルド時に指定する
     #[cfg(target_os = "macos")]
     let open_command = "open";
     #[cfg(target_os = "windows")]
