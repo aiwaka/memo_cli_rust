d191c5103c355525d646a571a2ac76158351c693
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-11-07 21:25:43 +0900

    file listing fix

--- src/commands/edit.rs
+++ src/commands/edit.rs
@@ -1,9 +1,8 @@
-use crate::{
-    editor::edit_with_vim, error::FileNotFoundError, memo_list::fuzzy_select_memo_or_default,
-};
+use crate::{editor::edit_with_vim, memo_list::fuzzy_select_memo_or_default};
 
-pub(super) fn edit_command(name: &Option<String>) -> Result<(), FileNotFoundError> {
-    let name = fuzzy_select_memo_or_default(name);
+pub(super) fn edit_command(name: &Option<String>) -> Result<(), Box<dyn std::error::Error>> {
+    let name = fuzzy_select_memo_or_default(name)?;
     // TODO: vim以外も使えるようにできたら嬉しい
-    edit_with_vim(name)
+    edit_with_vim(name)?;
+    Ok(())
 }
--- src/memo_list.rs
+++ src/memo_list.rs
@@ -29,41 +29,47 @@ pub(crate) fn memo_fullpath_list() -> Vec<(String, PathBuf)> {
 }
 
 /// ストレージ内のメモの名前の一覧を取得する
-pub(crate) fn memo_name_list() -> Vec<String> {
+/// 拡張子が.txtのもののみ取得する
+pub(crate) fn memo_name_list() -> Result<Vec<String>, std::io::Error> {
     let storage_dir = APP_CONFIG.get().unwrap().full_storage_dir.clone();
     let files = read_dir(storage_dir).unwrap();
-    files
+    let list = files
         .into_iter()
-        .map(|entry| {
-            entry
-                .unwrap()
-                .path()
-                .file_name()
-                .unwrap()
-                .to_string_lossy()
-                .strip_suffix(".txt")
-                .unwrap()
-                .to_string()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_type().ok()?.is_file() {
+                entry
+                    .file_name()
+                    .to_string_lossy()
+                    .into_owned()
+                    .strip_suffix(".txt")
+                    .map(|s| s.to_string())
+            } else {
+                None
+            }
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+    Ok(list)
 }
 
 /// メモ一覧からコンソールで曖昧検索する
-pub(crate) fn fuzzy_select_memo() -> String {
-    let name_list = memo_name_list();
+pub(crate) fn fuzzy_select_memo() -> Result<String, std::io::Error> {
+    let name_list = memo_name_list()?;
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select a memo")
         .default(0)
         .items(&name_list)
         .interact()
         .unwrap();
-    name_list[selection].clone()
+    Ok(name_list[selection].clone())
 }
 
 /// 名前を指定された場合にはそれを返し, されなかった場合には曖昧検索で決定する処理をまとめたもの
-pub(crate) fn fuzzy_select_memo_or_default(default: &Option<String>) -> String {
+pub(crate) fn fuzzy_select_memo_or_default(
+    default: &Option<String>,
+) -> Result<String, std::io::Error> {
     if let Some(name) = default {
-        name.clone()
+        Ok(name.clone())
     } else {
         fuzzy_select_memo()
     }
